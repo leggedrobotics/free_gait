99289d0f5eec114ca2b508faafdbf15f1690e53c
Peter Fankhauser <pfankhauser@ethz.ch>
2015-06-16 19:57:12 +0200

    Parameter tuning.

--- free_gait_core/src/StepCompleter.cpp
+++ free_gait_core/src/StepCompleter.cpp
@@ -16,10 +16,10 @@
 namespace free_gait {
 
 StepCompleter::StepCompleter()
-    : swingProfileDuration_(1.5),
-      swingProfileHeight_(0.1),
+    : swingProfileDuration_(2.0),
+      swingProfileHeight_(0.06),
       swingProfileType_("triangle"),
-      baseShiftProfileDuration_(1.0),
+      baseShiftProfileDuration_(2.0),
       baseShiftProfileHeight_(0.45),
       baseShiftProfileType_("linear"),
       surfaceNormal_(Vector::UnitZ())
