cefef6c5dbe311355ad5ee59f69c560010221f10
Peter Fankhauser <pfankhauser@ethz.ch>
2015-11-30 19:03:46 +0100

    Bugfix for uninitialized vector.

--- free_gait_core/src/step/StepCompleter.cpp
+++ free_gait_core/src/step/StepCompleter.cpp
@@ -88,7 +88,8 @@ bool StepCompleter::complete(const State& state, const Step& step, JointMotionBa
     jointMotion.updateStartAcceleration(startAcceleration);
   }
   if (jointMotion.getControlSetup().at(ControlLevel::Effort)) {
-    JointEfforts startEffort;// = JointEfforts::Zero();// TODO state.getJointEfforts(jointMotion.getLimb());
+    JointEfforts startEffort;//(JointEfforts::VectorBase::;// TODO state.getJointEfforts(jointMotion.getLimb());
+    startEffort.vector().resize(3);
     startEffort.setZero();
     jointMotion.updateStartEfforts(startEffort);
   }
