be690d36da44b1944e755808018322d8f0f9c840
Peter Fankhauser <pfankhauser@ethz.ch>
2016-12-19 15:51:11 +0100

    Added new float slider property for as RViz property type.

---
+++ free_gait_rviz_plugin/src/rviz/properties/float_slider_property.cpp
@@ -0,0 +1,48 @@
+/*
+ * float_slider_property.cpp
+ *
+ *  Created on: Dec 12, 2016
+ *  Author: Péter Fankhauser
+ *  Institute: ETH Zurich, Robotic Systems Lab
+ */
+
+#include "rviz/properties/float_slider_property.h"
+#include "rviz/properties/line_edit_with_slider.h"
+
+#include <QSlider>
+#include <QDoubleValidator>
+
+namespace rviz
+{
+
+FloatSliderProperty::FloatSliderProperty(const QString& name, float default_value,
+                               const QString& description, Property* parent,
+                               const char *changed_slot, QObject* receiver)
+    : FloatProperty(name, default_value, description, parent, changed_slot, receiver),
+      minIntValue_(0),
+      maxIntValue_(1000)
+{
+  setMin(0.0);
+  setMax(1.0);
+}
+
+void FloatSliderProperty::valueChanged(int value)
+{
+  const float floatValue = getMin() + (getMax() - getMin())
+      * (value - minIntValue_) / (maxIntValue_ - minIntValue_);
+  setValue(floatValue);
+}
+
+QWidget* FloatSliderProperty::createEditor(QWidget* parent, const QStyleOptionViewItem& option)
+{
+  LineEditWithSlider* slider = new LineEditWithSlider(parent);
+  slider->slider()->setRange(minIntValue_, maxIntValue_);
+  slider->setValidator(new QDoubleValidator(slider->slider()));
+  const int intValue = minIntValue_ + (maxIntValue_ - minIntValue_)
+      * (getValue().toFloat() - getMin()) / (getMax() - getMin());
+  slider->slider()->setValue(intValue);
+  connect(slider->slider(), SIGNAL(valueChanged(int)), this, SLOT(valueChanged(int)));
+  return slider;
+}
+
+} // end namespace rviz
---
+++ free_gait_rviz_plugin/src/rviz/properties/float_slider_property.h
@@ -0,0 +1,35 @@
+/*
+ * float_slider_property.h
+ *
+ *  Created on: Dec 12, 2016
+ *  Author: Péter Fankhauser
+ *  Institute: ETH Zurich, Robotic Systems Lab
+ */
+
+#pragma once
+
+#include <rviz/properties/float_property.h>
+#include <QString>
+#include <string>
+
+namespace rviz {
+
+class FloatSliderProperty : public FloatProperty
+{
+ Q_OBJECT
+ public:
+  FloatSliderProperty(const QString& name = QString(), float default_value = 0.0,
+                      const QString& description = QString(), Property* parent = 0,
+                      const char* changed_slot = 0, QObject* receiver = 0);
+
+  virtual QWidget* createEditor(QWidget* parent, const QStyleOptionViewItem& option);
+
+public Q_SLOTS:
+  void valueChanged(int value);
+
+ private:
+  int minIntValue_, maxIntValue_;
+};
+
+}  // end namespace rviz
+
