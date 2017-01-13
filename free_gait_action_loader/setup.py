9148ee46cb330334635a823fd2f1cd0db44cff3f
Samuel Bachmann <samuel.bachmann@gmail.com>
2017-01-13 12:00:54 +0100

    fix install

--- free_gait_action_loader/setup.py
+++ free_gait_action_loader/setup.py
@@ -3,7 +3,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['free_gait_action_loader'],
-    scripts=['bin/free_gait_action_loader'],
     package_dir={'': 'src'}
 )
 
