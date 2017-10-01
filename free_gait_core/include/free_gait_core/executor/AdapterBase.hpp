27dd0a77933a768eefae4fe68e0c41812791aa0c
Peter Fankhauser <pfankhauser@ethz.ch>
2017-10-01 16:25:04 +0200

    Compatibility with developments on ANYmal.

--- free_gait_core/include/free_gait_core/executor/AdapterBase.hpp
+++ free_gait_core/include/free_gait_core/executor/AdapterBase.hpp
@@ -109,7 +109,8 @@ class AdapterBase
       const LimbEnum& limb, const LinearAcceleration& endEffectorLinearAccelerationInWorldFrame) const = 0;
 
   //! Hook to write data to internal robot representation from state.
-  virtual bool setInternalDataFromState(const State& state) const = 0;
+  virtual bool setInternalDataFromState(const State& state, bool updateContacts = true, bool updatePosition = true,
+                                        bool updateVelocity = true, bool updateAcceleration = false) const = 0;
 };
 
 } /* namespace free_gait */
